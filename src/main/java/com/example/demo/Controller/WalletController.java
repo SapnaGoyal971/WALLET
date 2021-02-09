83d6f98a80f3debe0cc366ef7ca8ab3953fb66b2
SapnaGoyal971 <78151252+SapnaGoyal971@users.noreply.github.com>
2021-02-09 16:02:49 +0530

    Adding User's Project into wallet project

--- src/main/java/com/example/demo/Controller/WalletController.java
+++ src/main/java/com/example/demo/Controller/WalletController.java
@@ -3,11 +3,15 @@ package com.example.demo.Controller;
 import com.example.demo.Classes.RequestClass;
 import com.example.demo.Classes.Transaction;
 import com.example.demo.Classes.TransferDetails;
+import com.example.demo.Classes.UserClasses.User;
 import com.example.demo.Classes.Wallet;
+import com.example.demo.Repositories.UserRepository;
 import com.example.demo.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class WalletController {
@@ -15,10 +19,14 @@ public class WalletController {
     @Autowired
     WalletService walletService;
 
+    @Autowired
+    UserRepository userRepository;
 
 
     @RequestMapping(method = RequestMethod.POST, value = "/wallet")
     public String createWallet(@RequestBody RequestClass request){
+        if(userRepository.findByMobileNumber(request.getPhoneNumber()).isEmpty())
+            return "Phone Number does not exists.";
         try {
            Wallet wallet = walletService.createWallet(request);
             return "Wallet Created Successfully" + " with User Id: "+wallet.getUserId() ;
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.Classes.UserClasses.User;
+import com.example.demo.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void  createUser(User user) { userRepository.save(user); }
+
+    public List<User> readUser(Long userid){
+        List<User> us=new ArrayList<>();
+        userRepository.findById(userid).ifPresent(us::add);
+        return us;
+    }
+
+    public void updateUser(User user){
+        userRepository.save(user);
+    }
+
+    public void deleteUser(Long userid){
+        userRepository.deleteById(userid);
+    }
+
+}
